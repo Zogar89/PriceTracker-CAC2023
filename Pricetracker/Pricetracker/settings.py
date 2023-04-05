a20ff9590dd71bd9d0d8aafc276c76e7fe708a82
Zogar89 <Zogar89@users.noreply.github.com>
2023-04-05 15:55:09 -0300

    correcciones

--- Pricetracker/Pricetracker/settings.py
+++ Pricetracker/Pricetracker/settings.py
@@ -37,13 +37,14 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    
     'django.contrib.sites',
-
     'allauth',
     'allauth.account',
     'allauth.socialaccount',
+    'allauth.socialaccount.providers.google',
 ]
+
+SITE_ID = 1
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
