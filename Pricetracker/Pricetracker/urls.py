29950d612882732dc4f7534607dddc6d7f3a3c18
Zogar89 <Zogar89@users.noreply.github.com>
2023-04-13 16:58:42 -0300

    Agregado urls

--- Pricetracker/Pricetracker/urls.py
+++ Pricetracker/Pricetracker/urls.py
@@ -20,4 +20,6 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('accounts/', include('allauth.urls')),
+    path('tracks/',), #Listado de productos en seguimiento
+    path('product/',), #Página de producto
 ]
